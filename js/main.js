99e34dd3e8879a4a3688ec4970ee64e5927f9b06
Alexis <alkryzhanov@hotmail.com>
2020-08-19 00:10:58 +0300

    1. Added nav js 2. Added button up css/js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,39 @@
 'use strict';
 // Adding current year 
-$(document).ready(function () {
+$(document).ready(() => {
     let year = $('.footer__year');
     let currentYear = new Date().getFullYear();
 
     year.text(currentYear);
 
+});
+
+//ARROW button code block
+$(document).ready(() => {
+    let arrowUpBtn = $('.arrow-up__btn');
+
+    $(window).scroll(() => {
+        if ($(window).scrollTop() > 300) {
+            arrowUpBtn.addClass('show');
+        } else {
+            arrowUpBtn.removeClass('show');
+        }
+    });
+
+    arrowUpBtn.on('click', (evt) => {
+        evt.preventDefault();
+        $('html,body').animate({ scrollTop: 0 }, '1000');
+    });
+});
+
+//SMOOTH SITE navigation
+
+$(function () {
+    $('.navbar-nav a[href^="#"]').on('click', function () {
+        let target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 1000);
+        return false;
+    });
 });
